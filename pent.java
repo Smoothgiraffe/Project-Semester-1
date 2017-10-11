10652f6303a6b82ad84297bf45295f2b834a8df1
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2017-10-11 13:27:23 +0200

    Add files via upload

--- pent.java
+++ pent.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class pent{
 	public static void main(String[] args){
-    char[][] pentP = {{'p','p'},{'p','p'},{'p',0}}; //3x3 nxm
-		char[][] pentX = {{0,'x',0},{'x','x','x'},{0,'x',0}}; //3x3 mxm
-		char[][] pentF = {{0,'f','f'},{'f','f',0},{0,'f',0}};
-		char[][] pentV = {{'v',0,0},{'v',0,0},{'v','v','v'}};
-		char[][] pentW = {{'w',0,0},{'w','w',0},{0,'w','w'}};
-		char[][] pentY = {{0,'y'},{'y','y'},{0,'y'},{0,'y'}};
-		char[][] pentI = {{'i'},{'i'},{'i'},{'i'},{'i'}};
-		char[][] pentT = {{'t','t','t'},{0,'t',0},{0,'t',0}};
-		char[][] pentZ = {{'z','z',0},{0,'z',0},{0,'z','z'}};
-		char[][] pentU = {{'u',0,'u'},{'u','u','u'}};
-		char[][] pentN = {{'n','n',0,0},{0,'n','n','n'}};
-char[][] pentL = {{0,0,0,'l'},{'l','l','l','l'}};
+    char[][][] pentP = {{{'p','p'},{'p','p'},{'p',0}}}; //3x3 nxm
+		char[][][] pentX = {{{0,'x',0},{'x','x','x'},{0,'x',0}}}; //3x3 mxm
+		char[][][] pentF = {{{0,'f','f'},{'f','f',0},{0,'f',0}}};
+		char[][][] pentV = {{{'v',0,0},{'v',0,0},{'v','v','v'}}};
+		char[][][] pentW = {{{'w',0,0},{'w','w',0},{0,'w','w'}}};
+		char[][][] pentY = {{{0,'y'},{'y','y'},{0,'y'},{0,'y'}}};
+		char[][][] pentI = {{{'i'},{'i'},{'i'},{'i'},{'i'}}};
+		char[][][] pentT = {{{'t','t','t'},{0,'t',0},{0,'t',0}}};
+		char[][][] pentZ = {{{'z','z',0},{0,'z',0},{0,'z','z'}}};
+		char[][][] pentU = {{{'u',0,'u'},{'u','u','u'}}};
+		char[][][] pentN = {{{'n','n',0,0},{0,'n','n','n'}}};
+		char[][][] pentL = {{{0,0,0,'l'},{'l','l','l','l'}}};
 		char[] pentArray ={pentP, pentX, pentF, pentV, pentW, pentY, pentI, pentT, pentZ, pentU, pentN, pentL};
 
 		Scanner input = new Scanner(System.in);
@@ -29,10 +29,10 @@ char[][] pentL = {{0,0,0,'l'},{'l','l','l','l'}};
 		if(area%5 != 0){
 			System.out.println("Not possible.");
 		}
-		if else(area > 60){
+		else if(area > 60){
 			System.out.println("Definetely not possible!");
 		}
-		if else(grid[1][5] || grid[5][1]){ //????? or grid.length == 5 && grid[0].length == 1
+		else if(grid[1][5] || grid[5][1]){ //????? or grid.length == 5 && grid[0].length == 1
 			System.out.println("True."); //show??
 		}
 
